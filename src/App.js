a46b6005b86f543663f8915e6525a0123f80f919
lekimminhquan <lekimminhquan@gmail.com>
2024-06-06 20:00:00 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import './Todo'
+import { useEffect, useRef, useState } from 'react';
+import Todos from './Todo';
 function App() {
+  const [todo,setTodo] = useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+    <div className="form">
+        <div className="bodys">
+            <p>NEED TO DO</p>
+            <input type="text" placeholder="Add a new task" id="add" onChange={(e)=>setTodo(e.target.value)}/>
+            <button className='addbutton' >+</button>
+            <div className="status" >
+                <span id="ALL" >ALL</span>
+                <span id="TO DO" >To Do</span>
+                <span id="IN PROGRESS" >In Progressing</span>
+                <span id="DONE" >Done</span>
+                <button className="clearbutton">Clear</button>
+            </div>
+            <div className="items">
+                <ul className="ul">
+                  <Todos></Todos>
+                </ul> 
+            </div> 
+        </div>
     </div>
+  </>
   );
 }
 
