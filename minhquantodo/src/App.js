42e119216708b52ea9fecfa291a5e972926b0af0
minhquan18 <lekimminhquan@gmail.com>
2024-06-08 00:05:33 +0700

    update

--- minhquantodo/src/App.js
+++ minhquantodo/src/App.js
@@ -3,61 +3,76 @@ import './Todo'
 import { useEffect, useRef, useState } from 'react';
 import Todos from './Todo';
 import axios from 'axios';
-function App() {
-  const [todo,setTodo] = useState('')
-  const [task,setTask] =useState([])
-    const getapitask = async ()=>{
-        await axios.get('https://6641d7633d66a67b34352311.mockapi.io/api/todolist/1')
-        .then((res) =>{
-        res.data.map((item)=>{
-        setTask((e)=>[...e,item])
-      })
-    })
-  }
-  const postapi = async ()=>{
-    await axios.post('https://6641d7633d66a67b34352311.mockapi.io/api/todolist/1',{
-      item:todo,
-      status:'TO DO',
-    })
-  setTask((e)=>[...e,todo])
-    return(
-      <Todos task ={task}></Todos>
-    )
-  }
-  const delapi =  ()=>{
-    const checkeddel = document.querySelectorAll('.inputchecked:checked')
-    checkeddel.forEach(async (e)=>{
-      await axios.delete('https://6641d7633d66a67b34352311.mockapi.io/api/todolist/1/'+ e.id, )
-    } 
-  )
+
+function del() {
+  const checkeddel = document.querySelectorAll('.inputchecked:checked')
+  checkeddel.forEach((item) => {
+    axios.delete('https://6641d7633d66a67b34352311.mockapi.io/api/todolist/1/' + item.id)
+      .catch(err => { alert('bạn submit quá nhanh') })
+    removeelement(item.id)
+
+  })
 }
-   
-  useEffect(()=>{
+function removeelement(id) {
+  const x = document.getElementById(id)
+  console.log(id)
+  x.remove()
+}
+function App() {
+  const [todo, setTodo] = useState('')
+  const [task, setTask] = useState([])
+  const [tasktodo, setTasktodo] = useState([])
+
+  const getapitask = async () => {
+    await axios.get('https://6641d7633d66a67b34352311.mockapi.io/api/todolist/1')
+      .then((res) => {
+        res.data.map((item) => {
+          setTask((e) => [...e, item])
+        })
+      })
+  }
+  const postapi = async (todo) => {
+    if (todo != "") {
+      await axios.post('https://6641d7633d66a67b34352311.mockapi.io/api/todolist/1', {
+        item: todo,
+        status: 'TO DO',
+      })
+        .catch((error) => { alert('bạn đã submit quá nhanh') })
+      const clearinut = document.getElementById('add')
+      clearinut.value = ""
+      setTasktodo(...tasktodo,todo)
+      setTask([...task,todo])
+      setTodo('')
+    }
+  }
+
+
+  useEffect(() => {
     setTask([])
     getapitask()
-  },[])
+  }, [tasktodo])
   return (
-  <>
-    <div className="form">
+    <>
+      <div className="form">
         <div className="bodys">
-            <p>NEED TO DO</p>
-            <input type="text" placeholder="Add a new task" id="add" onChange={(e)=>setTodo(e.target.value)}/>
-            <button className='addbutton' onClick={postapi}>+</button>
-            <div className="status" >
-                <span id="ALL" >ALL</span>
-                <span id="TO DO" >To Do</span>
-                <span id="IN PROGRESS" >In Progressing</span>
-                <span id="DONE" >Done</span>
-                <button className="clearbutton" onClick={delapi}>Clear</button>
-            </div>
-            <div className="items">
-                <ul className="ul">
-                  <Todos task={task}></Todos>
-                </ul> 
-            </div> 
+          <p>NEED TO DO</p>
+          <input type="text" placeholder="Add a new task" id="add" onChange={(e) => setTodo(e.target.value)} />
+          <button className='addbutton' onClick={() => postapi(todo)}>+</button>
+          <div className="status" >
+            <span id="ALL" >ALL</span>
+            <span id="TO DO" >To Do</span>
+            <span id="IN PROGRESS" >In Progressing</span>
+            <span id="DONE" >Done</span>
+            <button className="clearbutton" onClick={del}>Clear</button>
+          </div>
+          <div className="items">
+            <ul className="ul">
+              <Todos task={task}></Todos>
+            </ul>
+          </div>
         </div>
-    </div>
-  </>
+      </div>
+    </>
   );
 }
 
--- minhquantodo/src/Todo.js
+++ minhquantodo/src/Todo.js
@@ -4,23 +4,25 @@ import './App.css';
 import memo from 'react';
 import axios from 'axios';
 
-function Todos(props){
-    return(
-       props.task.map((item)=>{
-        return(
-            <li className='firstcome' id={item.id}> 
-                <label>  
-                    <input type="checkbox" className='inputchecked' id={item.id}/>
-                    <p className="taskp">{item.item || item}</p>
-                    <span className="material-symbols-outlined" >edit</span>
-                    <select name="statustask"  className="roles" >
-                        <option className="opt" value="TO DO">TO DO</option>
-                        <option className="opt" value="IN PROGRESS">IN PROGRESS</option>
-                        <option className="opt" value="DONE">DONE</option>
-                    </select>
-                </label>
-            </li>
-        )
+function Todos(props) {
+
+    return (
+        props.task.map((item) => {
+            console.log(item.id)
+            return (
+                <li className='firstcome' id={item.id} key={item.id}>
+                    <label>
+                        <input type="checkbox" className='inputchecked' id={item.id} />
+                        <p className="taskp">{item.item || item}</p>
+                        <span className="material-symbols-outlined" >edit</span>
+                        <select name="statustask" className="roles" >
+                            <option className="opt" value="TO DO">TO DO</option>
+                            <option className="opt" value="IN PROGRESS">IN PROGRESS</option>
+                            <option className="opt" value="DONE">DONE</option>
+                        </select>
+                    </label>
+                </li>
+            )
         })
     )
 }
