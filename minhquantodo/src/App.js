2631738328d79a4bae3068c74cb9241770b2a66c
minhquan18 <lekimminhquan@gmail.com>
2024-06-09 17:33:10 +0700

    donetask

--- minhquantodo/src/App.js
+++ minhquantodo/src/App.js
@@ -22,7 +22,7 @@ function removeelement(id) {
 
 const App = () => {
   const [task, setTask] = useState([]);
-  const [role,setRole]=useState(0)
+  const [roles, setRole] = useState(Number)
 
   const getapitask = async () => {
     await axios
@@ -33,7 +33,40 @@ const App = () => {
         });
       });
   };
-
+  const getapitasktodo = async () => {
+    await axios
+      .get("https://6641d7633d66a67b34352311.mockapi.io/api/todolist/1")
+      .then((res) => {
+        res.data.map((e) => {
+          if (e.status == 'TO DO') {
+            setTask((t) => [...t, e]);
+          }
+        });
+      });
+  };
+  const getapitaskinprogress = async () => {
+    await axios
+      .get("https://6641d7633d66a67b34352311.mockapi.io/api/todolist/1")
+      .then((res) => {
+        res.data.map((e) => {
+          if (e.status == "IN PROGRESS") {
+            setTask((t) => [...t, e]);
+            console.log(e.status)
+          }
+        });
+      });
+  };
+  const getapitaskdone = async () => {
+    await axios
+      .get("https://6641d7633d66a67b34352311.mockapi.io/api/todolist/1")
+      .then((res) => {
+        res.data.map((e) => {
+          if (e.status == "DONE") {
+            setTask((t) => [...t, e]);
+          }
+        });
+      });
+  };
   const postapi = async (e) => {
     if (e.keyCode === 13) {
       if (e.target.value != "") {
@@ -74,25 +107,106 @@ const App = () => {
             item.key = false
             item.item = e.target.value
           }
-          setTask([...task])
+
         })
+        setTask([...task])
       }
       else {
         task.map((item) => {
           if (item.id == e.target.id) {
             item.key = false
           }
-          setTask([...task])
+
         })
+        setTask([...task])
       }
     }
   }
 
 
+  const clearshowrole = () => {
+    const clear = document.querySelectorAll('.role')
+    clear.forEach((clears) => {
+      clears.style.color = '#000'
+    })
+  }
+
+  const chooserole = () => {
+    switch (roles) {
+      case 0: {
+        clearshowrole()
+        const all = document.getElementById("ALL")
+        all.style.color = 'rgb(188, 188, 255)'
+        break
+      }
+      case 1: {
+        clearshowrole()
+        const all = document.getElementById("TO DO")
+        all.style.color = 'rgb(188, 188, 255)'
+        break
+      }
+      case 2: {
+        clearshowrole()
+        const all = document.getElementById("IN PROGRESS")
+        all.style.color = 'rgb(188, 188, 255)'
+        break
+      }
+      case 3: {
+        clearshowrole()
+        const all = document.getElementById("DONE")
+        all.style.color = 'rgb(188, 188, 255)'
+        break
+      }
+    }
+  }
+  const showseleterole = (statusname, value) => {
+    if (statusname == value) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
+
+  const setrole = async (e) => {
+    await axios.put('https://6641d7633d66a67b34352311.mockapi.io/api/todolist/1/' + e.target.id, {
+      status: e.target.value
+    })
+    task.map((item) => {
+      if (item.id == e.target.id) {
+        item.status = e.target.value
+      }
+    })
+    setTask([...task])
+  }
   useEffect(() => {
-    setTask([]);
-    getapitask();
-  }, []);
+    switch (roles) {
+      case 0: {
+        setTask([])
+        getapitask()
+        chooserole()
+        break
+      }
+      case 1: {
+        setTask([]);
+        getapitasktodo();
+        chooserole()
+        break
+      }
+      case 2: {
+        setTask([]);
+        getapitaskinprogress()
+        chooserole()
+        break
+      }
+      case 3: {
+        setTask([]);
+        getapitaskdone()
+        chooserole()
+        break
+      }
+    }
+  }, [roles]);
   return (
     <div className="form">
       <div className="bodys">
@@ -104,10 +218,10 @@ const App = () => {
           onKeyDown={postapi}
         />
         <div className="status">
-          <span id="ALL" className={`role${role==1 && "active"}`} onClick={()=>setRole(1)}>ALL</span>
-          <span id="TO DO" >To Do</span>
-          <span id="IN PROGRESS" >In Progressing</span>
-          <span id="DONE" >Done</span>
+          <span id="ALL" className="role" onClick={() => setRole(0)}>ALL</span>
+          <span id="TO DO" className="role" onClick={() => setRole(1)}>To Do</span>
+          <span id="IN PROGRESS" className="role" onClick={() => setRole(2)}>In Progressing</span>
+          <span id="DONE" className="role" onClick={() => setRole(3)}>Done</span>
           <button className="clearbutton" onClick={del}>
             Clear
           </button>
@@ -125,14 +239,14 @@ const App = () => {
                     />
                     {item.key ? <input className="add2" onKeyDown={editlisten} id={item.id}></input> : <p className="taskp" id={item.id}>{item.item}</p>}
                     <span className="material-symbols-outlined" onClick={() => edit(item.id)} >edit</span>
-                    <select name="statustask" className="roles">
-                      <option className="opt" value="TO DO">
+                    <select name="statustask" className="roles" id={item.id} onChange={setrole}>
+                      <option className="opt" value="TO DO" id={item.id} selected={showseleterole(item.status, 'TO DO')}>
                         TO DO
                       </option>
-                      <option className="opt" value="IN PROGRESS">
+                      <option className="opt" value="IN PROGRESS" id={item.id} selected={showseleterole(item.status, 'IN PROGRESS')}>
                         IN PROGRESS
                       </option>
-                      <option className="opt" value="DONE">
+                      <option className="opt" value="DONE" id={item.id} selected={showseleterole(item.status, 'DONE')}>
                         DONE
                       </option>
                     </select>
